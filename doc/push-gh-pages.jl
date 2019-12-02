b28b22509897977910743536fea8f3438fdec3f1
Avik Sengupta <avik@sengupta.net>
2019-12-02 15:09:57 +0000

    update doc system

--- doc/push-gh-pages.jl
+++ doc/push-gh-pages.jl
@@ -4,9 +4,9 @@ cd(@__DIR__)
 
 last_commit=readchomp(`git --no-pager log -1 --pretty=format:"%h:%s"`)
 
-ENV["GIT_DIR"]=abspath(chomp(readall(`git rev-parse --git-dir`)))
+ENV["GIT_DIR"]=abspath(chomp(read(`git rev-parse --git-dir`, String)))
 
-old_sha = chomp(readall(`git rev-parse refs/remotes/origin/gh-pages`))
+old_sha = chomp(read(`git rev-parse refs/remotes/origin/gh-pages`, String))
 
 run(`jekyll build`)
 
@@ -16,13 +16,13 @@ cd("_site") do
 	ENV["GIT_INDEX_FILE"]=gif
 	ENV["GIT_WORK_TREE"]=pwd()
 	run(`git add -A`)
-	tsha=chomp(readall(`git write-tree`))
+	tsha=chomp(read(`git write-tree`, String))
 	mesg="Deploy docs for master@$last_commit"
 
 	if length(old_sha) == 40
-	  	csha = chomp(readall(`git commit-tree $tsha -p $old_sha -m $(mesg)`))
+	  	csha = chomp(read(`git commit-tree $tsha -p $old_sha -m $(mesg)`, String))
 	else 
-		csha = chomp(readall(`git commit-tree $tsha -m $(mesg)`))
+		csha = chomp(read(`git commit-tree $tsha -m $(mesg)`, String))
 	end
 
 	 print("Created commit $csha")
