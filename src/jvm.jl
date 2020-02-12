ec245d6ffcb79b1cca81ff493c0982258e8ebc32
Avik Sengupta <avik@sengupta.net>
2020-02-12 14:53:25 +0000

    add more warnings

--- src/jvm.jl
+++ src/jvm.jl
@@ -152,6 +152,11 @@ end
 addOpts(s::String) = isloaded() ? @warn("JVM already initialised. This call has no effect") : push!(opts, s)
 
 function init()
+    if VERSION ≥ 1.1 && VERSION ≤ 1.2
+        @warn("JavaCall does not work well on Julia v$VERSION. \n " *
+                "Either use Julia v1.0.x, or v1.3.0 or higher.\n"*
+                "For 1.3 onwards, please also set the environment variable `JULIA_COPY_STACKS` to be `1` or `yes`")
+    end
     if VERSION ≥ v"1.3" && get(ENV, "JULIA_COPY_STACKS", "") ∉ ("1", "yes")
         @warn("JavaCall needs the environment variable `JULIA_COPY_STACKS` to be `1` or `yes`. "*
               "Calling the JVM may result in undefined behavior.")
