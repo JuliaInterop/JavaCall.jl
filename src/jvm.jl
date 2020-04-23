1cb34c4acfb3869ea8e201f5bc83c0a15f2cbefc
markkitt@gmail.com <markkitt@gmail.com>
2020-04-22 20:55:15 -0500

    Rewrite without reference to undocumented Base.Filesystem.path_separator

--- src/jvm.jl
+++ src/jvm.jl
@@ -151,12 +151,12 @@ function addClassPath(s::String)
         return
     end
     if s==""; return; end
-    if  endswith(s, "$(Base.Filesystem.path_separator)*.jar")
-        s=s[1:end-4]; # make it end with ".*" instead
-    end
-    if (endswith(s, "$(Base.Filesystem.path_separator)*") && isdir(s[1:end-2]))
-        for x in s[1:end-1] .* readdir(s[1:end-2])
-            endswith(x, ".jar") && push!(cp, x)
+    dirname, pattern = splitdir(s)
+    if pattern in ("*","*.jar") && isdir(dirname)
+        for filename in readdir(dirname)
+            if endswith(filename,".jar")
+                push!(cp, joinpath(dirname,filename))
+            end
         end
         return
     end
