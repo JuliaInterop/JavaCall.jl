d0a7e29b5b8cfcce8f09a01e854d6fdff3877bf7
Johannes Ahnlide <johannes@voxel.se>
2021-04-25 13:55:07 +0200

    Better interface, finalizer, fixes

--- src/jniarray.jl
+++ src/jniarray.jl
@@ -8,39 +8,60 @@ jniname(::Type{jfloat}) = "Float"
 jniname(::Type{jdouble}) = "Double"
 
 
-mutable struct JNIArray{T,N}
-    ptr
-    arr::Union{Nothing,Array{T,N}}
+mutable struct JNIArray{T} <: AbstractVector{T}
+    ref::JavaRef
+    arr::Union{Nothing,Vector{T}}
+    function JNIArray{T}(ref) where T
+        j = new{T}(ref, nothing)
+        finalizer(deleteref, j)
+        return j
+    end
 end
 
-JNIArray(ptr) = get_elements!(JNIArray)
-signature(::Type{JNIArray{T,N}}) where {T,N} = string("[" ^ N, signature(T))
-jvalue(jarr::JNIArray) = jarr.ptr
+Base.getindex(jarr::JNIArray, args...) = getindex(jarr.arr, args...)
+Base.setindex!(jarr::JNIArray, args...) = setindex!(jarr.arr, args...)
+Base.size(jarr::JNIArray, args...; kwargs...) = size(jarr.arr, args...; kwargs...)
+
+function deleteref(x::JNIArray{T}) where T
+    if !isnothing(x.arr)
+        release_elements(x)
+    end
+    deleteref(x.ref)
+    x.ref = J_NULL
+end
+
+signature(::Type{JNIArray{T}}) where T = string("[", signature(T))
+jvalue(jarr::JNIArray) = jarr.ref.ptr
+JNIArray{T}(ptr::Ptr{Nothing}) where {T} = JNIArray{T}(JavaLocalRef(ptr))
 
 for primitive in [:jboolean, :jchar, :jbyte, :jshort, :jint, :jlong, :jfloat, :jdouble]
     name = jniname(eval(primitive))
     get_elements = :(JNI.$(Symbol("Get$(name)ArrayElements")))
     release_elements = :(JNI.$(Symbol("Release$(name)ArrayElements")))
+    new_array = :(JNI.$(Symbol("New$(name)Array")))
     m = quote
-        function get_elements!(jarr::JNIArray{$primitive, N}) where N
-            sz = Int(JNI.GetArrayLength(jarr.ptr))
-            jarr.arr = unsafe_wrap(Array, $get_elements(jarr.ptr, Ptr{jboolean}(C_NULL)), sz)
+        function get_elements!(jarr::JNIArray{$primitive})
+            sz = Int(JNI.GetArrayLength(jarr.ref.ptr))
+            jarr.arr = unsafe_wrap(Array, $get_elements(jarr.ref.ptr, Ptr{jboolean}(C_NULL)), sz)
             jarr
         end
-        function convert_result(::Type{JNIArray{$primitive, N}}, ptr) where N
-            get_elements!(JNIArray{$primitive,N}(ptr, nothing))
-        end
-        function convert_arg(argtype::Type{Vector{$primitive}}, arg::JNIArray{$primitive,1}) where N
-            $release_elements(arg.ptr, pointer(arg.arr), jint(0))
+        JNIArray{$primitive}(sz::Int) = get_elements!(JNIArray{$primitive}($new_array(sz)))
+        function release_elements(arg::JNIArray{$primitive})
+            $release_elements(arg.ref.ptr, pointer(arg.arr), jint(0))
             arg.arr = nothing
-            return arg, arg.ptr
         end
-        function convert_arg(argtype::Type{JNIArray{$primitive,1}}, arg::JNIArray{$primitive,1}) where N
-            $release_elements(arg.ptr, pointer(arg.arr), jint(0))
-            arg.arr = nothing
+        function convert_result(::Type{JNIArray{$primitive}}, ptr)
+            get_elements!(JNIArray{$primitive}(ptr))
+        end
+        function convert_arg(argtype::Type{Vector{$primitive}}, arg::JNIArray{$primitive})
+            release_elements(arg)
             return arg, arg
         end
-        function cleanup_arg(jarr::JNIArray{$primitive, N}) where N
+        function convert_arg(argtype::Type{JNIArray{$primitive}}, arg::JNIArray{$primitive})
+            release_elements(arg)
+            return arg, arg
+        end
+        function cleanup_arg(jarr::JNIArray{$primitive})
             get_elements!(jarr)
         end
     end
