40000f394cb314954c74b8caf03667881be5a9eb
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2021-05-21 14:25:36 +0100

    Improved find methods test to check for one specific method.

--- test/reflection.jl
+++ test/reflection.jl
@@ -16,6 +16,10 @@
         method
     end
 
+    function jni_newstring(vector::Vector{Char})
+        JNI.new_string(map(JNI.jchar, vector), length(vector))
+    end
+
     @testset "Find class" begin
         class = Reflection.findclass(Symbol("java.lang.String"))
         @test_not_cnull class
@@ -33,5 +37,29 @@
         methods = Reflection.classmethods(Symbol("java.lang.String"))
         @test_not_cnull methods
         @test_isa methods JNI.jobjectArray
+
+        found_getbytes = false
+
+        getname_id = jni_getmethodid(
+            jni_findclass("java/lang/reflect/Method"),
+            "getName",
+            "()Ljava/lang/String;")
+        equals_id = jni_getmethodid(
+            jni_findclass("java/lang/String"),
+            "equals",
+            "(Ljava/lang/Object;)Z"
+        )
+
+        expected_name = jni_newstring(['g', 'e', 't', 'B', 'y', 't', 'e', 's'])
+
+        num_methods = JNI.get_array_length(methods)
+        for i in 1:num_methods
+            m = JNI.get_object_array_element(methods, i-1)
+            m_name = JNI.call_object_method_a(m, getname_id, JNI.jvalue[])
+            if JNI.call_boolean_method_a(m_name, equals_id, JNI.jvalue[expected_name]) == true
+                found_getbytes = true
+            end
+        end
+        @test found_getbytes
     end
 end
