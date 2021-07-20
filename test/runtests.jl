7cc0fbfd5dbdb49b4a8f5e8d0df57e44d9ff0934
Johannes Ahnlide <johannes@voxel.se>
2021-07-20 14:40:37 +0200

    expand tests for fields and exceptions

--- test/runtests.jl
+++ test/runtests.jl
@@ -33,6 +33,8 @@ System = @jimport java.lang.System
 System_out = jfield(System, "out", @jimport java.io.PrintStream )
 @info "Java Version: ", jcall(System, "getProperty", JString, (JString,), "java.version")
 
+@testset "JavaCall" begin
+
 @testset "unsafe_strings_1" begin
     a=JString("how are you")
     @test Ptr(a) != C_NULL
@@ -92,11 +94,60 @@ end
     @test typeof(h)==jint
 end
 
+@testset "exceptions_1" begin
+    j_u_arrays = @jimport java.util.Arrays
+    j_math = @jimport java.lang.Math
+    j_is = @jimport java.io.InputStream
+    
+    # JavaCall.JavaCallError("Error calling Java: java.lang.ArithmeticException: / by zero")
+    @test_throws JavaCall.JavaCallError jcall(j_math, "floorDiv", jint, (jint, jint), 1, 0)
+    @test isnothing(JavaCall.geterror())
+
+    # JavaCall.JavaCallError("Error calling Java: java.lang.ArrayIndexOutOfBoundsException: Array index out of range: -1")
+    @test_throws JavaCall.JavaCallError jcall(j_u_arrays, "sort", Nothing, (Array{jint,1}, jint, jint), [10,20], -1, -1)
+    @test isnothing(JavaCall.geterror())
+
+    # JavaCall.JavaCallError("Error calling Java: java.lang.IllegalArgumentException: fromIndex(1) > toIndex(0)")
+    @test_throws JavaCall.JavaCallError jcall(j_u_arrays, "sort", Nothing, (Array{jint,1}, jint, jint), [10,20], 1, 0)
+    @test isnothing(JavaCall.geterror())
+end
+
+@testset "fields_1" begin
+    JTest = @jimport(Test)
+    t=JTest(())
+    t_fields = Dict(getname(f) => f for f in listfields(t))
+
+    @testset "$ftype" for (name, ftype, valtest) in [ ("booleanField", jboolean, ==(true)) ,
+                                ("integerField", jint, ==(100)) ,
+                                ("stringField", JString, ==("A STRING")) ,
+                                ("objectField", JObject, x -> Ptr(x) == C_NULL) ]
+        @test valtest(jfield(t, name, ftype))
+        @test valtest(t_fields[name](t))
+        @test valtest(jfield(t, t_fields[name]))
+    end
+
+    @test jfield(@jimport(java.lang.Math), "E", jdouble) == 2.718281828459045
+    @test jfield(@jimport(java.lang.Math), "PI", jdouble) == 3.141592653589793
+    @test jfield(@jimport(java.lang.Byte), "MAX_VALUE", jbyte) == 1<<7-1
+    @test jfield(@jimport(java.lang.Integer), "MAX_VALUE", jint) == 1<<31-1
+    @test jfield(@jimport(java.lang.Long), "MAX_VALUE", jlong) == 1<<63-1
+
+    j_l_bool = @jimport(java.lang.Boolean)
+    @test jcall(jfield(j_l_bool, "TRUE", j_l_bool), "booleanValue", jboolean, ()) == true
+    @test jcall(jfield(j_l_bool, "FALSE", j_l_bool), "booleanValue", jboolean, ()) == false
+
+    @test jfield(@jimport(java.text.NumberFormat), "INTEGER_FIELD", jint) == 0
+    locale = @jimport java.util.Locale
+    lc = jfield(locale, "CANADA", locale)
+    @test jcall(lc, "getCountry", JString, ()) == "CA" #TODO: remove?
+    #Instance field access
+end
+
 #Test NULL
 @testset "null_1" begin
     H=@jimport java.util.HashMap
     a=jcall(T, "testNull", H, ())
-    @test_throws ErrorException jcall(a, "toString", JString, ())
+    @test_throws JavaCall.JavaCallError jcall(a, "toString", JString, ())
 
     jlist = @jimport java.util.ArrayList
     @test jcall( jlist(), "add", jboolean, (JObject,), JObject(C_NULL)) === 0x01
@@ -171,15 +222,8 @@ end
     t=JTest(())
     inner = TestInner((JTest,), t)
     @test jcall(inner, "innerString", JString, ()) == "from inner"
-    @test jfield(@jimport(java.lang.Math), "E", jdouble) == 2.718281828459045
-    @test jfield(@jimport(java.lang.Math), "PI", jdouble) == 3.141592653589793
-    @test jfield(@jimport(java.text.NumberFormat), "INTEGER_FIELD", jint) == 0
-    Locale = @jimport java.util.Locale
-    lc = jfield(@jimport(java.util.Locale), "CANADA", @jimport(java.util.Locale))
-    #Instance field access
     #Disabled for now. Need to verify stability
     @test jfield(@jimport(java.util.logging.Logger), "GLOBAL_LOGGER_NAME", JString ) == "global"
-    @test jcall(lc, "getCountry", JString, ()) == "CA"
     @test jfield(t, "integerField", jint) == 100
     @test jfield(t, "stringField", JString) == "A STRING"
 end
@@ -317,6 +361,8 @@ end
     @test_throws ErrorException jlocalframe(Nothing) do 
         error("Error within jlocalframe f")
     end
+end
+
 end
 
 # Test downstream dependencies
